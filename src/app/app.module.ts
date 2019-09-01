456a62165d40ce057f2270f0b39b9e401d011919
uwishemajustine <alsenjsatine90@gmail.com>
2019-09-01 17:45:22 +0200

    create repository class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component'
 import { NavbarComponent } from './navbar/navbar.component';
+import { RepositoryComponent } from './repository/repository.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
     NavbarComponent,
+    RepositoryComponent,
     
   ],
   imports: [
