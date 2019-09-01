0623374046a7755feb332cefb0dba62e23949e29
uwishemajustine <alsenjsatine90@gmail.com>
2019-09-01 15:15:18 +0200

    add class profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
-import { ProfileRequest } from './profile-request.service/profile-request.service.component';
+import { UsersComponent } from './user/user.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent,
-    ProfileRequest.ServiceComponent,
+    UsersComponent,
     NavbarComponent
   ],
   imports: [
