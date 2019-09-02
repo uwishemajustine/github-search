58d061e3fc10eee7b12aaf349e1ff8c120cb8b6c
uwishemajustine <alsenjsatine90@gmail.com>
2019-09-02 10:09:15 +0200

    changes in pipedate

--- src/app/pipes/pipe-date.pipe.ts
+++ src/app/pipes/pipe-date.pipe.ts
@@ -5,9 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class PipeDatePipe implements PipeTransform {
 
-  transform(value: any): string  {
+  transform(value: any): any  {
 
-    return value.topipeDate;
-}
-
-}
+        let valueArr = value.replace(/(\d{4})-(\d{2})-(\d{2}).*/ig, "$1 $2 $3").split(" ");
+        let dateCreated = new Date(valueArr[0], valueArr[1] - 1, valueArr[2]);
+        let now = new Date();
+        let diff = now.getTime() - dateCreated.getTime();
+        diff /= (1000 * 60 * 60 * 24);
+        return Math.floor(diff)
+      }
+     }
